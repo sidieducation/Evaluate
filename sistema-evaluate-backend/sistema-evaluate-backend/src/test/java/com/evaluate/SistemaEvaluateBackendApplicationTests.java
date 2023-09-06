3031bfaa9575127a1dbb669c7fb430cfd2b93a2a
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-09-06 18:47:12 +0200

    Create proyect backend

---
+++ sistema-evaluate-backend/sistema-evaluate-backend/src/test/java/com/evaluate/SistemaEvaluateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEvaluateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
