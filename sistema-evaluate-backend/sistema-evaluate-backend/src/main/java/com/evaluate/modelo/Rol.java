8594a175a9bc2aa4f1a5a52dd543ade2035b085b
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-09-06 20:26:56 +0200

    I have generated getters and setters in the three classes, in user, in Role and in Role user. I have tested the compilation and it works very well, hibernate gives me the mysql data when compiling.

--- sistema-evaluate-backend/sistema-evaluate-backend/src/main/java/com/evaluate/modelo/Rol.java
+++ sistema-evaluate-backend/sistema-evaluate-backend/src/main/java/com/evaluate/modelo/Rol.java
@@ -15,4 +15,33 @@ public class Rol {
 
     @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "rol")
     private Set<UsuarioRol> usuarioRoles = new HashSet<>();
+
+
+    public Long getRolId() {
+        return rolId;
+    }
+
+    public void setRolId(Long rolId) {
+        this.rolId = rolId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Set<UsuarioRol> getUsuarioRoles() {
+        return usuarioRoles;
+    }
+
+    public void setUsuarioRoles(Set<UsuarioRol> usuarioRoles) {
+        this.usuarioRoles = usuarioRoles;
+    }
+
+    public Rol(){
+
+    }
 }
